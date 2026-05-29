116b0f6be10f1b71e777c37b650b4bf317a1cb9b
iker daniel solorio burgos <20253ds052@utez.edu.mx>
2026-05-29 14:29:58 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const btnSaludar  = document.getElementById('btn-saludar');
+let mensajeMostrado =false;
+console.log(btnSaludar);
 
+btnSaludar.addEventListener('click',function(){
+
+const seccionContacto = document.getElementById('Contacto');
+    console.log('click')
+    if(!mensajeMostrado){
+    const mensaje = document.createElement('p');
+    mensaje.textContent='gracias por tu visita';
+    mensaje.style.color= '#2f5a6a';
+    mensaje.style.fontWeight='bold';
+
+    seccionContacto.appendChild(mensaje);
+    mensajeMostrado=true;
+    btnSaludar.textContent="mensaje enviado"
+    btnSaludar.disabled=true;
+    btnSaludar.style.backgroundColor='#bdc3c7'
+    }
+
+})
